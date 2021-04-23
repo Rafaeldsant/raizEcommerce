13a50585212b7ab2a031e995761bc65d92620a1e
Rafaeldsant <rafaelcamposdsant@gmail.com>
2021-04-22 23:43:57 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -38,7 +39,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var imagens_list_aux = [];
 
-
   TextEditingController editingController = TextEditingController();
 
   void _incrementCounter() {
@@ -50,36 +50,63 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: myDrawer(),
       backgroundColor: Colors.white,
-      appBar:
-      PreferredSize(
-        //Here is the preferred height.
-        preferredSize: Size.fromHeight(80.0),
-        child: SafeArea(
-          child: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              flexibleSpace: Center(child:
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                Container(child: Image.asset('assets/logo_verde.png')),
-              ]),
-              )
-          ),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: global.cor_primaria),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(child: Image.asset('assets/logo_verde.png', scale: 1.5)),
+          ]),
         ),
+        actions: [
+          Padding(padding: EdgeInsets.all(5), child:
+          CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: AssetImage('assets/outros/avatar.jpg'),
+          )
+          )
+        ],
       ),
-      body:
-      SingleChildScrollView(child:
-        Container(
-            child: Column(children: [
-              searchBar(),
-              destaqueSection(),
-              sectionComponent('Feminino', 'feminino'),
-              sectionComponent('Masculino', 'masculino'),
-              sectionComponent('Marcas Brasileiras', 'mb')
-            ])
-        ),
+      body: SingleChildScrollView(
+        child: Container(child: Column(children: [searchBar(), destaqueSection(), sectionComponent('Feminino', 'feminino'), sectionComponent('Masculino', 'masculino'), sectionComponent('Marcas Brasileiras', 'mb')])),
+      ),
+    );
+  }
+
+  myDrawer() {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Drawer Header'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
       ),
     );
   }
@@ -95,7 +122,7 @@ class _MyHomePageState extends State<MyHomePage> {
           decoration: InputDecoration(
               border: InputBorder.none,
               filled: true,
-              fillColor: Color(0xffF7F8FC),
+              fillColor: Color(0xffE9E9E9),
               hintText: "Busca Raiz",
               hintStyle: TextStyle(color: Colors.grey[400]),
               contentPadding: const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 15.0),
@@ -106,8 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
               enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(25.7)),
               prefixIcon: Icon(Icons.search, color: global.cor_primaria),
               suffixIcon: GestureDetector(
-                onTap: () {
-                },
+                onTap: () {},
                 child: Icon(Icons.arrow_forward, color: global.cor_primaria),
               )),
         ),
@@ -115,28 +141,16 @@ class _MyHomePageState extends State<MyHomePage> {
     ]);
   }
 
-  destaqueSection () {
+  destaqueSection() {
+    var destaques_list_aux = getDestaquesConteudo();
 
-   var destaques_list_aux = getDestaquesConteudo();
+    print(destaques_list_aux);
 
-   print(destaques_list_aux);
-
-    return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Container(
-              padding: EdgeInsets.all(12),
-              child:
-            Text('Destaques', textScaleFactor: 2, style: TextStyle(fontWeight: FontWeight.bold))
-          ),
-          SizedBox(height: 20),
-          Container(
-          height: 100,
-          child: ListView(scrollDirection: Axis.horizontal, children: [
-            for (int i = 0; i < destaques_list_aux.length; i++)
-              destaquesCards(destaques_list_aux[i], i)
-          ]))
-      ]);
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(padding: EdgeInsets.all(12), child: Text('Destaques', textScaleFactor: 2, style: TextStyle(fontWeight: FontWeight.bold))),
+      SizedBox(height: 20),
+      Container(height: 100, child: ListView(scrollDirection: Axis.horizontal, children: [for (int i = 0; i < destaques_list_aux.length; i++) destaquesCards(destaques_list_aux[i], i)]))
+    ]);
   }
 
   getDestaquesConteudo() {
@@ -148,15 +162,13 @@ class _MyHomePageState extends State<MyHomePage> {
     destaques_list.add({"titulo": "Livros", "icon": FontAwesomeIcons.book});
     destaques_list.add({"titulo": "Artes", "icon": FontAwesomeIcons.palette});
 
-
     var cores_principais = ['0xffC80092', '0xff00401C', '0xff401903', '0xffD45B15'];
     var cores_secundarias = ['0xff8C0E6A', '0xff00602C', '0xff5B2104', '0xffFF9658'];
 
     int cor_index = 0;
 
     for (int i = 0; i < destaques_list.length; i++) {
-
-      if(cor_index > 3) {
+      if (cor_index > 3) {
         cor_index = 0;
       }
 
@@ -179,26 +191,25 @@ class _MyHomePageState extends State<MyHomePage> {
                 alignment: Alignment.bottomCenter,
                 children: [
                   Container(
-                      width: 60.0,
-                      height: 60.0,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            Color(int.parse(dados_destaques['cor1'])),
-                            Color(int.parse(dados_destaques['cor2'])),
-                          ],
-                        ),
-                        //border: Border.all(width: 3, color: AppConfig.of(context).corPrincipal),
-                        shape: BoxShape.circle,
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(int.parse(dados_destaques['cor1'])),
+                          Color(int.parse(dados_destaques['cor2'])),
+                        ],
                       ),
+                      //border: Border.all(width: 3, color: AppConfig.of(context).corPrincipal),
+                      shape: BoxShape.circle,
+                    ),
                     child: Center(child: FaIcon(dados_destaques['icon'], color: Colors.white)),
                   ),
                 ],
               ),
-              onTap: () {
-              }),
+              onTap: () {}),
           SizedBox(height: 5),
           Row(children: [
             Expanded(child: Text(dados_destaques['titulo'].toString(), textScaleFactor: 0.85, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800]), textAlign: TextAlign.center, maxLines: 2)),
@@ -207,33 +218,21 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   sectionComponent(String titulo, String tipo) {
-
     imagens_list_aux = getImagens(tipo);
 
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              margin: EdgeInsets.only(top: 12),
-              padding: EdgeInsets.all(12),
-              child:
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(titulo, textScaleFactor: 1.8, style: TextStyle(fontWeight: FontWeight.bold)),
-                    Row(children: [
-                      Text('Ver tudo',  textScaleFactor: 1.2, style: TextStyle(color: global.cor_secundaria, fontWeight: FontWeight.bold)),
-                      Icon(Icons.arrow_forward_ios, size: 15, color: global.cor_secundaria)
-                    ])
-                  ])
-          ),
-          photoGrid(imagens_list_aux)
-        ]);
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(
+          margin: EdgeInsets.only(top: 12),
+          padding: EdgeInsets.all(12),
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(titulo, textScaleFactor: 1.8, style: TextStyle(fontWeight: FontWeight.bold)),
+            Row(children: [Text('Ver tudo', textScaleFactor: 1.2, style: TextStyle(color: global.cor_secundaria, fontWeight: FontWeight.bold)), Icon(Icons.arrow_forward_ios, size: 15, color: global.cor_secundaria)])
+          ])),
+      photoGrid(imagens_list_aux)
+    ]);
   }
 
-
   photoGrid(List imagens) {
-
     if (imagens.length > 0)
       return Container(
           height: 180,
@@ -241,18 +240,47 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(width: 12),
             Wrap(direction: Axis.vertical, children: [
               for (int i = 0; i < imagens.length; i++)
-                InkWell(
-                  child: Container(
-                    decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.grey[300], blurRadius: 2.0, offset: Offset(0, 2), spreadRadius: 0.0)], borderRadius: BorderRadius.all(Radius.circular(6)), color: Colors.white),
-                    margin: EdgeInsets.all(3),
-                    width: 120,
-                    height: 80,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)),
-                        child: Image.asset(imagens[i]['img'], fit: BoxFit.cover,))
-                  ),
-                  onTap: () {
-                  },
+                Stack(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.grey[300], blurRadius: 2.0, offset: Offset(0, 2), spreadRadius: 0.0)], borderRadius: BorderRadius.all(Radius.circular(8)), color: Colors.white),
+                        margin: EdgeInsets.all(3),
+                        width: 120,
+                        height: 80,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(6)),
+                            child: Image.asset(
+                              imagens[i]['img'],
+                              fit: BoxFit.cover,
+                            ))),
+                    if (imagens[i]['titulo'] != null && imagens[i]['titulo'] != "")
+                      InkWell(
+                        child: Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(begin: FractionalOffset.topCenter, end: FractionalOffset.bottomCenter, colors: [
+                                Colors.grey.withOpacity(0.0),
+                                Colors.black.withOpacity(0.7),
+                              ], stops: [
+                                0.0,
+                                1.0
+                              ]),
+                              borderRadius: BorderRadius.all(Radius.circular(6)),
+                              color: Colors.white),
+                          margin: EdgeInsets.all(3),
+                          width: 120,
+                          height: 80,
+                          child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                imagens[i]['titulo'],
+                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+                                textAlign: TextAlign.center,
+                              )),
+                        ),
+                        onTap: () {},
+                      ),
+                  ],
                 )
             ])
           ]));
@@ -261,39 +289,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   getImagens(String tipo) {
-
     imagens_list.clear();
 
-    if(tipo == 'feminino') {
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/oculos.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/saias.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/sapatos.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/saltos.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/feminino/vestidos.png", "rota": "page_comercio_antiga"});
-    }
-
-    else if (tipo == 'masculino'){
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/masculino/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/masculino/bermudas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/masculino/tenis.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/masculino/bones.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/categorias/masculino/calcas.png", "rota": "page_comercio_antiga"});
-    }
-
-    else {
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
-      imagens_list.add({"titulo": "Comércio e Serviços", "img": "assets/camisas.png", "rota": "page_comercio_antiga"});
+    if (tipo == 'feminino') {
+      imagens_list.add({"titulo": "Camisas", "img": "assets/categorias/feminino/camisas.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Óculos", "img": "assets/categorias/feminino/oculos.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Saias", "img": "assets/categorias/feminino/saias.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Sapatos", "img": "assets/categorias/feminino/sapatos.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Saltos", "img": "assets/categorias/feminino/saltos.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Vestidos", "img": "assets/categorias/feminino/vestidos.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Acessórios", "img": "assets/categorias/feminino/acessorios.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Chapéus", "img": "assets/categorias/feminino/chapeus.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Cosméticos", "img": "assets/categorias/feminino/cosmeticos.png", "rota": "page_comercio_antiga"});
+    } else if (tipo == 'masculino') {
+      imagens_list.add({"titulo": "Camisas", "img": "assets/categorias/masculino/camisas.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Bermudas", "img": "assets/categorias/masculino/bermudas.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Tênis", "img": "assets/categorias/masculino/tenis.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Bonés", "img": "assets/categorias/masculino/bones.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Calças", "img": "assets/categorias/masculino/calcas.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Casacos", "img": "assets/categorias/masculino/casacos.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "Beleza", "img": "assets/categorias/masculino/beleza.png", "rota": "page_comercio_antiga"});
+    } else {
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
+      imagens_list.add({"titulo": "", "img": "assets/categorias/mb/mb_default.png", "rota": "page_comercio_antiga"});
     }
 
     return imagens_list;
-
   }
-
 }
